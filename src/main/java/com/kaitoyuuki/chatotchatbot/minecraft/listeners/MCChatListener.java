7817b1236950982619bbea6536c216e148607a95
Ian Finley <ianfinleyhome@gmail.com>
2016-10-12 18:30:22 -0400

    updated to use new config object, some general cleanup; added command listener to copy /me commands to discord chat.

--- src/main/java/com/kaitoyuuki/chatotchatbot/minecraft/listeners/MCChatListener.java
+++ src/main/java/com/kaitoyuuki/chatotchatbot/minecraft/listeners/MCChatListener.java
@@ -1,10 +1,14 @@
 package com.kaitoyuuki.chatotchatbot.minecraft.listeners;
 
 import com.kaitoyuuki.chatotchatbot.ChatotChatbot;
-import com.kaitoyuuki.chatotchatbot.discord.bot;
+import com.kaitoyuuki.chatotchatbot.discord.Bot;
+import net.minecraft.command.PlayerNotFoundException;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraftforge.event.CommandEvent;
 import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -14,6 +18,7 @@ import org.apache.logging.log4j.Logger;
 import static com.kaitoyuuki.chatotchatbot.minecraft.ChatUtils.formatMCforMC;
 import static com.kaitoyuuki.chatotchatbot.minecraft.ChatUtils.stripAmp;
 import static com.kaitoyuuki.chatotchatbot.minecraft.ChatUtils.stripFormat;
+import static net.minecraft.command.CommandBase.getChatComponentFromNthArg;
 
 @SuppressWarnings("unused")
 public class MCChatListener {
@@ -26,16 +31,16 @@ public class MCChatListener {
         String message = event.getMessage();
 
         //TODO pass chat through ChatUtils to modify appearance appropriately
-        if (ChatotChatbot.override) {
+        if (ChatotChatbot.config.doesOverride()) {
             event.setComponent(new TextComponentString(formatMCforMC(event)));
         }
         //removes all minecraft formatting sets (ampersand and section sign) before passing to discord.
-        //this should eventually be replaced, and probably won't be helpful
+        //this should eventually be replaced with discord formatting conversion, and probably won't be helpful
         message = stripFormat(message);
         message = stripAmp(message);
         String dcM = "**" + player + ":** " + message + "";
         lg.debug(dcM);
-        bot.instance.sendMessage(dcM);
+        Bot.instance.sendMessage(dcM);
 
     }
 
@@ -44,7 +49,7 @@ public class MCChatListener {
         String player = event.player.getDisplayNameString();
         String dcM = "_**" + player + "** has joined the game_";
         lg.debug(dcM);
-        bot.instance.sendMessage(dcM);
+        Bot.instance.sendMessage(dcM);
     }
 
     @SubscribeEvent
@@ -52,7 +57,7 @@ public class MCChatListener {
         String player = event.player.getDisplayNameString();
         String dcM = "_**" + player + "** has left the game_";
         lg.debug(dcM);
-        bot.instance.sendMessage(dcM);
+        Bot.instance.sendMessage(dcM);
     }
 
 
@@ -60,12 +65,33 @@ public class MCChatListener {
     public void onPlayerDeath(LivingDeathEvent event) {
         if (event.getEntityLiving() instanceof EntityPlayerMP) {
             EntityPlayer player = (EntityPlayer)event.getEntityLiving();
-            String ded = player.getDisplayNameString();
-            String death = player.getCombatTracker().getDeathMessage().getUnformattedText();
-            String dcM = "_**" + ded + "** " + death + "_";
+            String cadaver = player.getDisplayNameString();
+            String deathMsg = player.getCombatTracker().getDeathMessage().getUnformattedText();
+            String dcM = "_**" + cadaver + "** " + deathMsg + "_";
             lg.debug(dcM);
-            bot.instance.sendMessage(dcM);
+            Bot.instance.sendMessage(dcM);
 
+
+        }
+    }
+
+    @SubscribeEvent
+    public void onCommand(CommandEvent event) {
+
+
+        if(event.getCommand().getCommandName().equals("me")) {
+            try {
+                ITextComponent itextcomponent = getChatComponentFromNthArg(event.getSender(), event.getParameters(), 0, !(event.getSender() instanceof EntityPlayer));
+                TextComponentTranslation tct = new TextComponentTranslation("chat.type.emote", event.getSender().getDisplayName(), itextcomponent);
+                String emote = tct.getUnformattedText();
+
+                String dcM = "_" + emote + "_";
+                lg.debug(dcM);
+                Bot.instance.sendMessage(dcM);
+
+            } catch (PlayerNotFoundException e) {
+                //do nothing. because it's not impossible.
+            }
         }
     }
 
