efc0d9e42ee9b87b0529f351fc4c433b5f3676b1
Ian Finley <ianfinleyhome@gmail.com>
2016-10-12 13:23:18 -0400

    fixed modid to match convention

--- src/main/java/com/kaitoyuuki/chatotchatbot/ChatotChatbot.java
+++ src/main/java/com/kaitoyuuki/chatotchatbot/ChatotChatbot.java
@@ -13,7 +13,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = ChatotChatbot.MODID, version = ChatotChatbot.VERSION, acceptableRemoteVersions = "*")
 public class ChatotChatbot {
 
-    public static final String MODID = "ChatotChatbot";
+    public static final String MODID = "chatotchatbot";
     public static final String VERSION = "1.0";
 
     public static boolean ENABLED;
