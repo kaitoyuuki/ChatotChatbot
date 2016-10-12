07646b1e5c3c33414db358e6628a892753012943
Ian Finley <ianfinleyhome@gmail.com>
2016-10-12 18:28:43 -0400

    updated to use new config object, some general cleanup; removed unnecessary listeners; modified order of command listening

--- src/main/java/com/kaitoyuuki/chatotchatbot/discord/listeners/DCChatListener.java
+++ src/main/java/com/kaitoyuuki/chatotchatbot/discord/listeners/DCChatListener.java
@@ -2,7 +2,7 @@ package com.kaitoyuuki.chatotchatbot.discord.listeners;
 
 
 import com.kaitoyuuki.chatotchatbot.ChatotChatbot;
-import com.kaitoyuuki.chatotchatbot.discord.bot;
+import com.kaitoyuuki.chatotchatbot.discord.Bot;
 import com.kaitoyuuki.chatotchatbot.discord.handler.CDCommand;
 import com.kaitoyuuki.chatotchatbot.minecraft.handler.MCChatHandler;
 import net.dv8tion.jda.events.message.guild.GuildMessageReceivedEvent;
@@ -10,52 +10,27 @@ import net.dv8tion.jda.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.events.guild.*;
 import net.dv8tion.jda.hooks.ListenerAdapter;
 
-import static com.kaitoyuuki.chatotchatbot.minecraft.ChatUtils.formatDCforMC;
-
 
 public class DCChatListener extends ListenerAdapter {
 
-    static MCChatHandler mcc = new MCChatHandler();
-
-    //Joined a new guild(server)
-    @Override
-    public void onGuildJoin(GuildJoinEvent event)
-    {
-
-        ChatotChatbot.log.info("I joined a new Guild! Guild Name: " + event.getGuild().getName());
-
-    }
-
-    //left a guild
-    @Override
-    public void onGuildLeave(GuildLeaveEvent event)
-    {
-
-        ChatotChatbot.log.info("I left a guild... Hope I wasn't kicked.  Guild Name: " + event.getGuild().getName());
-
-    }
-
-    // private messages
-    @Override
-    public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
-        ChatotChatbot.log.info("[pm]<" + event.getAuthor().getUsername() + ">:" + event.getMessage().getContent());
-
-
-    }
+    private static MCChatHandler mcc = new MCChatHandler();
 
     //channel messages
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         ChatotChatbot.log.info("[" + event.getGuild().getName() + "][" + event.getChannel().getName() + "]<" +
                 event.getAuthor().getUsername() + ">:" + event.getMessage().getContent());
-        if (event.getGuild().getId().equals(ChatotChatbot.GID)) {
-            if (event.getChannel().getId().equals(ChatotChatbot.CID)) {
-                if(event.getMessage().getContent().startsWith(ChatotChatbot.PREFIX)) {
-                    CDCommand.handle(event.getChannel(), event.getAuthor(), event.getMessage().getContent().substring(1));
+        if (event.getGuild().getId().equals(ChatotChatbot.config.getGID())) {
+            if (event.getChannel().getId().equals(ChatotChatbot.config.getCID())) {
+                if (!(event.getAuthor().equals(Bot.instance.getMe()))) {
+                    if(event.getMessage().getContent().startsWith(ChatotChatbot.config.getPREFIX())) {
+                        if (!(event.getAuthor().isBot())) {
+                            CDCommand.handle(event.getChannel(), event.getAuthor(), event.getMessage().getContent().substring(1));
+                        }
+                    } else {
 
-                } else if (!(event.getAuthor().equals(bot.instance.getMe()))) {
-
-                    mcc.sendMessage(event);
+                        mcc.sendMessage(event);
+                    }
                 }
             }
         }
